d06ba09c6ecdc7a09c5a81c5aa495ce67ed76a37
DeivinsonMina <160056079+DeivinsonMina@users.noreply.github.com>
2025-05-30 21:49:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -787,7 +787,7 @@ def obtener_respuesta_openai(mensaje):
 def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT")),
+        port=3306,
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
